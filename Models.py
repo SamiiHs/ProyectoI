5f62ee033c74de499ebc2ab00dadc1ed9938a80c
SamiiHs <80808866+SamiiHs@users.noreply.github.com>
2021-03-26 14:42:39 -0500

    Update Models.py

--- Models.py
+++ Models.py
@@ -35,40 +35,6 @@ class LossHistory(Callback):
         self.losses.append(logs.get('loss'))
         self.val_losses.append(logs.get('val_loss'))
 
-
-def train_Bi_LSTM(X, Y, epochs = 30, validation_split = 0.2, patience=20):
-    speed_input = Input(shape = (X.shape[1], X.shape[2]), name = 'signal')
-    
-    main_output = Bidirectional(LSTM(4,input_shape = (X.shape[1], X.shape[2]), return_sequences=False), merge_mode='ave')(speed_input)
-    
-    final_model = Model([speed_input], [main_output])
-    
-    final_model.summary()
-    
-    final_model.compile(loss='mse', optimizer='adam')
-    
-    history = LossHistory()
-    earlyStopping = EarlyStopping(monitor='val_loss', min_delta=0.00001, patience=patience, verbose=0, mode='auto')
-    final_model.fit([X], Y,epochs = epochs, validation_split = 0.2, callbacks=[history, earlyStopping])
-    
-    return final_model, history
-
-def train_2_Bi_LSTM_mask(X, Y, epochs = 30, validation_split = 0.2, patience=20):
-    
-    model = Sequential()
-    model.add(Masking(mask_value=0.,input_shape=(X.shape[1], X.shape[2])))
-    model.add(LSTM(4, return_sequences=True, input_shape = (X.shape[1], X.shape[2])))
-    model.add(LSTM(4, return_sequences=False, input_shape = (X.shape[1], X.shape[2])))
-
-    model.add(Dense(4))
-    model.compile(loss='mse', optimizer='rmsprop')
-
-    history = LossHistory()
-    earlyStopping = EarlyStopping(monitor='val_loss', min_delta=0.00001, patience=patience, verbose=0, mode='auto')
-    model.fit(X, Y, validation_split = 0.2, epochs = epochs, callbacks=[history, earlyStopping])
-
-    return model, history
-
 def train_2_Bi_LSTM(X, Y, epochs = 30, validation_split = 0.2, patience=20):
     signal = Input(shape = (6000, 4),dtype=np.float32, name = 'signal')
     init = signal
@@ -89,5 +55,4 @@ def train_2_Bi_LSTM(X, Y, epochs = 30, validation_split = 0.2, patience=20):
     history = LossHistory()
     earlyStopping = EarlyStopping(monitor='val_loss', min_delta=0.00001, patience=patience, verbose=0, mode='auto')
     h=final_model.fit([X], Y, epochs = epochs,validation_split = 0.2, callbacks=[history, earlyStopping])
-    print(h)
     return final_model, history
